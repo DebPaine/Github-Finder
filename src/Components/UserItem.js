89ab0c1dfbdb494d49c579e5606b557ab7a52d2d
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-05-12 21:00:29 +0530

    removed redundant code

--- src/Components/UserItem.js
+++ src/Components/UserItem.js
@@ -7,9 +7,6 @@ export default class UserItem extends Component {
     return (
       <div className='card text-center'>
         <h2>{this.props.user}</h2>
-        <h2>{login}</h2>
-        <h2>{avatar}</h2>
-        <h2>{url}</h2>
       </div>
     );
   }
